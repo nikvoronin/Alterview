bc8d446565708063b10a62d5b26bc9bce2e51407
nikvoronin <nvoronin@outlook.com>
2019-12-03 22:08:18 +0300

    Add core project structure and base data models: Sport, SportEvent

---
+++ src/Alterview.Core/Models/Sport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Alterview.Core.Models
+{
+    public class Sport
+    {
+        [Key]
+        public int SportId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Alterview.Core/Models/SportEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Alterview.Core.Models
+{
+    public class SportEvent
+    {
+        [Key]
+        public int EventId { get; set; }
+        public int SportId { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public decimal Team1Price { get; set; }
+        public decimal DrawPrice { get; set; }
+        public decimal Team2Price { get; set; }
+    }
+}
