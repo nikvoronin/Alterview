bed980b6ef397292fd2f05c0449537038cdf8911
nikvoronin <nvoronin@outlook.com>
2019-12-03 22:46:10 +0300

    Add stubs of the database repositories

---
+++ src/Alterview.Infrastructure/Data/EventsRepository.cs
@@ -0,0 +1,36 @@
+﻿using Alterview.Core.Interfaces;
+using Alterview.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alterview.Infrastructure.Data
+{
+    public class EventsRepository : RepositoryBase, IEventsRepository
+    {
+        public EventsRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public Task<SportEvent> GetEventById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SportEvent>> GetEventsBySportAndDate(int sportId, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int InitializeSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateEvent(SportEvent ev)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Alterview.Infrastructure/Data/SportsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Alterview.Core.Interfaces;
+using Alterview.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alterview.Infrastructure.Data
+{
+    public class SportsRepository : RepositoryBase, ISportsRepository
+    {
+        public SportsRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public Task<IEnumerable<SportInfo>> GetSportsWithEventsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int InitializeSchema()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
