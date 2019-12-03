70797f5d8cabbf0627219f49dec0397b49558e68
nikvoronin <nvoronin@outlook.com>
2019-12-03 22:24:30 +0300

    Add sports repository interface. Add SportInfo data model

---
+++ src/Alterview.Core/Interfaces/ISportsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Alterview.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alterview.Core.Interfaces
+{
+    public interface ISportsRepository
+    {
+        Task<IEnumerable<SportInfo>> GetSportsWithEventsCount();
+    }
+}
---
+++ src/Alterview.Core/Models/SportInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alterview.Core.Models
+{
+    public class SportInfo : Sport
+    {
+        public int EventsCount { get; set; }
+    }
+}
