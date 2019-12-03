0f3c064b36cc18bfe80e89aa3ae2224488b9a066
nikvoronin <nvoronin@outlook.com>
2019-12-03 22:28:12 +0300

    Add events repository interface

---
+++ src/Alterview.Core/Interfaces/IEventsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Alterview.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alterview.Core.Interfaces
+{
+    public interface IEventsRepository
+    {
+        Task<SportEvent> GetEventById(int id);
+        Task<IEnumerable<SportEvent>> GetEventsBySportAndDate(int sportId, DateTime date);
+        Task<int> UpdateEvent(SportEvent ev);
+    }
+}
