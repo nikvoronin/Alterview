d125fce09d96550aeca3f69506015dfef12506dd
nikvoronin <nvoronin@outlook.com>
2019-12-08 12:34:17 +0300

    Add data channel interface

---
+++ src/Alterview.Core/Interfaces/IDataChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alterview.Core.Interfaces
+{
+    public interface IDataChannel<T>
+    {
+        int Id { get; }
+        bool PushData(T data);
+    }
+}
